0e322fafbe73c21cda494ba4f5a2a74145b670b1
TaYYKaYY <107534876+TaYYKaYY@users.noreply.github.com>
2023-01-22 01:27:15 +0200

    .

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { useState } from "react";
+import List from "./components/List";
+
+export default function App(){
+    const [value, setValue] = useState([])
+    const [inputValue, setInputValue] = useState('')
+    function showText(event){
+        const date = new Date()
+        let {value, name} = event.target.previousSibling
+        if (value === ''){
+            return
+        }
+        setValue(prevValue => [...prevValue, {id: prevValue.length + 1, [name]: value, hours: date.getHours(), minutes:date.getMinutes()}])
+        setInputValue('')
+        event.target.previousSibling.focus()
+    }
+    console.log(value)
+    function updateText(event) {
+        setInputValue(event.target.value)
+    }
+    function deleteItem(event) {
+        const {textContent} = event.target.parentElement.children[1]    
+        const arr = value.find(obj => obj.name === textContent)
+        value.forEach(item => {
+            if (item === arr){
+                const index = value.findIndex(e => e === item)
+                setValue(prevValue => {
+                    prevValue.splice(index, 1)
+                    return [...prevValue]
+                })
+            }
+        })
+    }
+    const allNames = value.map(name => <List 
+        key={name.id}
+        name={name.name}
+        id={name.id}
+        click={deleteItem}
+        hours={name.hours}
+        minutes={name.minutes}
+        />)
+    return (
+        <form onSubmit={(event) => event.preventDefault()}>
+            <h1>To-Do List</h1>
+            <div className="upper">
+                <div>
+                    <input type="text" name="name" value={inputValue} onChange={updateText} placeholder="Tasks..." autoFocus/>
+                    <button className="upper-btn fa-solid fa-plus" onClick={showText} type="submit"></button>
+                    {value.length > 0 && <button className="upper-btn clear" onClick={() => setValue([])}>clear all</button>}
+                </div>
+                {value.length > 0 && <p className="counter">You have {value.length} task{value.length > 1 ? 's' : ''}</p>}
+            </div>
+            <div className="flex">
+                {value[0] ? 
+                 <ul>
+                        <li>{allNames}</li>    
+                    </ul> :
+                    <p>No Tasks Yet!</p>
+             }
+            </div>
+        </form>
+    )
+}
---
+++ src/components/List.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+
+export default function List(props) {
+    const [checked, setChecked] = useState(false)
+    function check() {
+        setChecked(prevChecked => !prevChecked)
+    }
+    return (
+        <div>
+            <div className="list-item">
+                <input type="checkbox" checked={checked} onChange={check}  id={props.id}/>
+                <label for={props.id} className={`list--name ${checked && "checked"}`}>{props.name}</label>
+                <button onClick={props.click} className="delete-btn fa-solid fa-trash"></button>
+                <p className="time">
+                    {props.hours}:{props.minutes < 10 ? `0${props.minutes}` : props.minutes} {props.hours < 12 ? "AM" : "PM"}
+                </p>
+            </div>
+        
+        </div>
+    )
+}
